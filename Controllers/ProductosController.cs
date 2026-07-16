841745dd051ccd61758ce0618ba6ab957125b611
rubsega <rubseg182gmail.com>
2026-07-16 13:54:44 -0500

    Edit y Delete incluidos, Delete es un modal en vez de un view entero, asi que solo se hace un POST

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -37,4 +37,43 @@ public class ProductosController : Controller
         }
         return View(producto);
     }
+    public async Task<IActionResult> Edit(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+        return View(producto);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Producto producto)
+    {
+        if (id != producto.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(producto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(producto);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto == null) return NotFound();
+
+        _context.Productos.Remove(producto);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
 }
